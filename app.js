117b9801481ad21b709559f566357257212244ed
David Tang <dtang859@gmail.com>
2016-11-13 21:31:10 -0800

    week 2 on node

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var Sequelize = require('sequelize');
 var cors = require('cors'); // Cross Origin Resource Sharing
+var bodyParser = require('body-parser');
 
 var DB_NAME = 'music';
 var DB_USER = 'student';
@@ -15,6 +16,9 @@ var Song = sequelize.define('song', {
   title: {
     type: Sequelize.STRING
   },
+  price: {
+    type: Sequelize.DECIMAL
+  },
   playCount: {
     type: Sequelize.INTEGER,
     field: 'play_count'
@@ -24,10 +28,52 @@ var Song = sequelize.define('song', {
 });
 
 app.use(cors());
+app.use(bodyParser());
 // app.use(function(request, response, next) {
 //   response.header('Access-Control-Allow-Origin', '*');
 //   next();
 // });
+
+app.put('/api/songs/:id', function(request, response) {
+  Song.findById(request.params.id).then(function(song) {
+    if (song) {
+      song.update({
+        title: request.body.title
+      }).then(function(song) {
+        response.json(song);
+      });
+    } else {
+      response.status(404).json({
+        message: 'Song not found'
+      });
+    }
+  });
+});
+
+app.post('/api/songs', function(request, response) {
+  // response.json(request.body);
+  var song = Song.build({
+    title: request.body.title,
+    price: request.body.price
+  });
+  song.save().then(function(song) {
+    response.json(song);
+  });
+});
+
+app.delete('/api/songs/:id', function(request, response) {
+  Song.findById(request.params.id).then(function(song) {
+    if (song) {
+      song.destroy().then(function(song) {
+        response.json(song);
+      });
+    } else {
+      response.status(404).json({
+        message: 'Song not found'
+      });
+    }
+  });
+});
 
 app.get('/api/songs', function (request, response) {
   var promise = Song.findAll();
@@ -36,6 +82,6 @@ app.get('/api/songs', function (request, response) {
       data: songs
     });
   });
-})
+});
 
-app.listen(3000)
+app.listen(3000);
